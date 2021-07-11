dd2e7d2cab5d15c52ba7625588120cf70a13337f
Alec Heckert <alecheckert@gmail.com>
2021-07-11 15:01:46 -0700

    rewrote spagl.utils.concat_tracks for better handling of empty arguments

--- spagl/utils.py
+++ spagl/utils.py
@@ -66,7 +66,7 @@ def assign_index_in_track(tracks):
 def concat_tracks(*tracks):
     """
     Join some trajectory dataframes together into a larger dataframe,
-    while preserving uniqe trajectory indices.
+    while preserving unique trajectory indices.
 
     args
     ----
@@ -77,38 +77,14 @@ def concat_tracks(*tracks):
         pandas.DataFrame, the concatenated trajectories
 
     """
-    n = len(tracks)
+    tracks = [t.assign(dataframe_idx=i) for i, t in enumerate(tracks) if not t.empty]
+    inc = 0
+    for i, t in enumerate(tracks):
+        tracks[i].loc[tracks[i]['trajectory'] >= 0, 'trajectory'] += inc
+        inc = tracks[i]['trajectory'].max()+1
+    return pd.concat(tracks, axis=0, ignore_index=True, sort=False) \
+        if len(tracks)>0 else pd.DataFrame(dtype=object)
 
-    # Sort the tracks dataframes by their size. The only important thing
-    # here is that if at least one of the tracks dataframes is nonempty,
-    # we need to put that one first.
-    df_lens = [len(t) for t in tracks]
-    try:
-        tracks = [t for _, t in sorted(zip(df_lens, tracks))][::-1]
-    except ValueError:
-        pass
-
-    # Iteratively concatenate each dataframe to the first while 
-    # incrementing the trajectory index as necessary
-    out = tracks[0].assign(dataframe_index=0)
-    c_idx = out["trajectory"].max() + 1
-
-    for t in range(1, n):
-
-        # Get the next set of trajectories and keep track of the origin
-        # dataframe
-        new = tracks[t].assign(dataframe_index=t)
-
-        # Ignore negative trajectory indices (facilitating a user filter)
-        new.loc[new["trajectory"]>=0, "trajectory"] += c_idx 
-
-        # Increment the total number of trajectories
-        c_idx = new["trajectory"].max() + 1
-
-        # Concatenate
-        out = pd.concat([out, new], ignore_index=True, sort=False)
-
-    return out
 
 #############################
 ## TRACK LOADING UTILITIES ##
